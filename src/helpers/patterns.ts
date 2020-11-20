39981d03bd27e93ecd6ea1d4feb3ad7df8dc3998
Austral1a <lienglex@gmail.com>
2020-11-20 10:57:58 +0200

    add: patterns and patternNames

---
+++ src/helpers/patterns.ts
@@ -0,0 +1,11 @@
+export const patterns = {
+    name: /^[a-zA-Z]+$/,
+    email: /^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$/,
+    password: /\w{6,}/
+};
+
+export const patternNames = {
+    name: 'name',
+    email: 'email',
+    password: 'password'
+};
