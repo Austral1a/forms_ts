7db4ae427ecdf23ac15754b660fc16c7d1963df8
Austral1a <lienglex@gmail.com>
2020-11-20 17:24:46 +0200

    formik

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,5 +1,5 @@
 import React, {ChangeEvent, FC, useEffect, useState} from 'react'
-import {ErrorMessage, Formik, Form, Field, useFormik} from 'formik'
+import {ErrorMessage, Formik, Form, Field, useFormik, useField, FormikProps} from 'formik'
 import {Input, Button} from '../Components'
 import {useValidations, useLoginFormManagement} from './hooks'
 import './Login.scss'
@@ -7,6 +7,18 @@ import './Login.scss'
 interface FormErrors {
     isEmailValid?: boolean;
     isPasswordValid?: boolean;
+}
+
+// @ts-ignore
+const TextField = ({...props}) => {
+    // @ts-ignore
+    const [field, meta] = useField(props)
+    return (
+        <div className='input-container'>
+            <input className='input-container__input' {...field} {...props} />
+            {meta.touched && meta.error ? <p className='input-container__error'>{meta.error}</p> : null}
+        </div>
+    )
 }
 
 export const Login: FC = () => {
@@ -36,42 +48,58 @@ export const Login: FC = () => {
         } = useLoginFormManagement()
     */
 
+/*
     const formik = useFormik({
         initialValues: {email: '', password: ''},
         onSubmit: () => console.log('submitted'),
         validate: useValidations
     })
+*/
 
     // const isBtnDisabled: boolean = Object.values(formErrors).includes(false)
+
+    interface Values {
+        email: string;
+        password: string;
+    }
+
+    const validate = (values: Values) => {
+        // TODO: Re-write useValidations
+        const errors: Values = {email: '', password: ''};
+        if (!values.password) {
+            errors.password = 'Required';
+        } else if (values.password.length > 15) {
+            errors.password = 'Must be 15 characters or less';
+        }
+
+        if (!values.email) {
+            errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+        }
+
+        return errors;
+    };
+
     return (
-        <Form onSubmit={formik.handleSubmit} className='form-login'>
-            <Input
-                name='email'
-                type='email'
-                placeholder='Email'
-                // onBlur={handleEmailBlur}
-                onChange={formik.handleChange}
-                value={formik.values.email}
-                hasError={!!formik.errors.email}
-                errorMessage={formik.errors.email}
-            />
-            <Input
-                name='password'
-                type='password'
-                placeholder='Password'
-                // onBlur={handlePasswordBlur}
-                onChange={formik.handleChange}
-                value={formik.values.password}
-                hasError={!!formik.errors.password}
-                errorMessage={formik.errors.password}
-            />
-            <Button
-                type='submit'
-                text='Login'
-                onClick={() => console.log('logged in')}
-                disabled={formik.isValid}
-            />
-        </Form>
+        <Formik
+            initialValues={{email: '', password: ''}}
+            onSubmit={() => console.log('submitted')}
+            validate={validate}
+        >
+            {(props: FormikProps<Values>) => (
+                <Form className='form-login'>
+                    <TextField name='email' type='text' placeholder='Email' />
+                    <TextField name='password' type='password' placeholder='Password' />
+                    <Button
+                        type='submit'
+                        text='Login'
+                        onClick={() => console.log(props.isValid)}
+                        disabled={!!props.errors.email || !!props.errors.password}
+                    />
+                </Form>
+            )}
+        </Formik>
     )
 
 /*
--- src/Login/hooks/useInputValidation.tsx
+++ src/Login/hooks/useInputValidation.tsx
@@ -4,6 +4,6 @@ import {patterns, isInputValid} from '../../helpers'
 export const useInputValidation = (patternName: string, inputName: string): boolean => {
     return useMemo(
         () => isInputValid(patterns[patternName], inputName),
-        [isInputValid, inputName, patternName, patterns]
+        [inputName, patternName]
     )
 }
