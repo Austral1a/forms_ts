e02b0be53c0630a8aaf0ad513d523c4bacb30898
Austral1a <lienglex@gmail.com>
2020-11-20 11:13:14 +0200

    add: interfaces for patterns and patternNames

--- src/helpers/patterns.ts
+++ src/helpers/patterns.ts
@@ -1,10 +1,18 @@
-export const patterns = {
+interface Patterns {
+    [key: string]: RegExp
+}
+
+interface Names {
+    [key: string]: string
+}
+
+export const patterns: Patterns = {
     name: /^[a-zA-Z]+$/,
     email: /^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$/,
-    password: /\w{6,}/
+    password: /\w{6,}/,
 };
 
-export const patternNames = {
+export const patternNames: Names = {
     name: 'name',
     email: 'email',
     password: 'password'
