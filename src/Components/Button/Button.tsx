17c5cd1e9a66ccf6a9df15a4e179509231a96adc
Austral1a <lienglex@gmail.com>
2020-11-20 10:43:18 +0200

    add: Buttom component

---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,24 @@
+import React, {FC, MouseEventHandler} from 'react';
+import classNames from 'classnames/dedupe';
+import './Button.scss';
+
+interface Props {
+    text: string;
+    disabled?: boolean;
+    className?: string;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export const Button: FC<Props> = ({
+    text,
+    onClick,
+    disabled,
+    className
+}) => {
+    const customClasses: string = classNames('button', className);
+    return (
+        <button className={customClasses} onClick={onClick} disabled={disabled}>
+            {text}
+        </button>
+    );
+}
