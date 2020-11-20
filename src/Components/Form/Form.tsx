79c8188f01f52feec6a0b65479b72b8d476622b0
Austral1a <lienglex@gmail.com>
2020-11-20 10:47:07 +0200

    add: Form Component

---
+++ src/Components/Form/Form.tsx
@@ -0,0 +1,30 @@
+import React, {FC, FormEventHandler, ReactNode} from 'react';
+import classNames from 'classnames/dedupe';
+import './Form.scss';
+
+interface Props {
+    className?: string;
+    isValid?: boolean;
+    action?: string;
+    children?: ReactNode;
+    onSubmit?: FormEventHandler<HTMLFormElement>;
+}
+
+export const Form: FC<Props> = ({
+    children,
+    onSubmit,
+    action,
+    className,
+    isValid
+}) => {
+    const customClasses = classNames('form', className, {'form_success': isValid});
+    return(
+        <form
+            className={customClasses}
+            action={action}
+            onSubmit={onSubmit}
+        >
+            {children}
+        </form>
+    );
+}
