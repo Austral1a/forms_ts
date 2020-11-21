418722556ae4a532fcae3d8fb648c7b0ea0f2ff2
Dan Korzh <lienglex@gmail.com>
2020-11-21 13:44:20 +0200

    add: password's icon management hook

---
+++ src/Login/hooks/usePasswordIconManagement.tsx
@@ -0,0 +1,24 @@
+import { useCallback, useState } from "react";
+import { pswd_visibility_off, pswd_visibility_on } from "../../assets";
+
+interface Result {
+  setPswdIcon: string;
+  pswdInputType: string;
+  handlePswdIconMouseDown: () => void;
+  handlePswdIconMouseUp: () => void;
+}
+
+export const usePasswordIconManagement = (): Result => {
+  const [isPswdMouseDown, setIsPsdwMouseDown] = useState(false);
+
+  return {
+    setPswdIcon: isPswdMouseDown ? pswd_visibility_on : pswd_visibility_off,
+    pswdInputType: isPswdMouseDown ? "text" : "password",
+    handlePswdIconMouseDown: useCallback(() => setIsPsdwMouseDown(true), [
+      setIsPsdwMouseDown,
+    ]),
+    handlePswdIconMouseUp: useCallback(() => setIsPsdwMouseDown(false), [
+      setIsPsdwMouseDown,
+    ]),
+  };
+};
