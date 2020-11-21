24259082eec5250bc09dee927b54e16272d1e017
Dan Korzh <lienglex@gmail.com>
2020-11-21 13:45:30 +0200

    add: svgs

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+import email from "./email.svg";
+import pswd_visibility_on from "./pswd_visibility_on.svg";
+import pswd_visibility_off from "./pswd_visibilty_off.svg";
+export { email, pswd_visibility_on, pswd_visibility_off };
