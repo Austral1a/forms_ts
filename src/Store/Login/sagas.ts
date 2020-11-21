b8de98dcc57219c3abdb32e6577f721c2cef2472
Dan Korzh <lienglex@gmail.com>
2020-11-21 18:10:27 +0200

    add: Login's sagas

---
+++ src/Store/Login/sagas.ts
@@ -0,0 +1,29 @@
+import { call, put, takeEvery } from "redux-saga/effects";
+
+import { LOGIN, LOGIN_SUCCESS, LOGIN_FAIL } from "./actions";
+import { login as loginProcess } from "../../API";
+
+interface Action {
+  type: string;
+  payload: { email: string; password: string; errorMessage: string };
+}
+
+// worker
+export function* login(action: Action) {
+  try {
+    const {
+      payload: { email, password },
+    } = action;
+    yield call(loginProcess, email, password);
+    yield put({
+      type: LOGIN_SUCCESS,
+      payload: { email, password },
+    });
+  } catch (e) {
+    yield put({ type: LOGIN_FAIL, payload: { errorMessage: e.message } });
+  }
+}
+
+export function* loginRequest() {
+  yield takeEvery(LOGIN, login);
+}
