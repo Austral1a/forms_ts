3565391241896d517674252b603e3b622f4ca1ea
Dan Korzh <lienglex@gmail.com>
2020-11-21 13:46:05 +0200

    del: useless hooks

--- src/Login/hooks/useInputValidation.tsx
+++
@@ -1,9 +0,0 @@
-import {useMemo} from 'react'
-import {patterns, isInputValid} from '../../helpers'
-
-export const useInputValidation = (patternName: string, inputName: string): boolean => {
-    return useMemo(
-        () => isInputValid(patterns[patternName], inputName),
-        [inputName, patternName]
-    )
-}
