752085a574b6bc7a8626d065ed44342ab428ca16
Dan Korzh <lienglex@gmail.com>
2020-11-21 18:09:11 +0200

    add: additional action types

--- src/Store/Login/actions.ts
+++ src/Store/Login/actions.ts
@@ -1,4 +1,6 @@
 export const LOGIN: string = "LOGIN";
+export const LOGIN_SUCCESS: string = "LOGIN_SUCCESS";
+export const LOGIN_FAIL: string = "LOGIN_FAIL";
 
 export const loginAction = (email: string, password: string) => ({
   type: LOGIN,
