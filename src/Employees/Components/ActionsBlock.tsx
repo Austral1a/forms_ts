ded147f1abd16b9407a0a524f71c3b2a449dfdcb
Austral1a <lienglex@gmail.com>
2020-11-24 14:28:20 +0200

    replaced internal logic by custom hook

--- src/Employees/Components/ActionsBlock.tsx
+++ src/Employees/Components/ActionsBlock.tsx
@@ -2,37 +2,41 @@ import React, { FC, ReactElement, useState } from "react";
 import { Button } from "../../Components";
 import { EditEmployeeModal, DeleteEmployeeModal } from "./index";
 import { GetEmployee } from "../../Interfaces/Employees";
+import { useActionsBlockManagement } from "./hooks";
 
 interface Props {
   employeeData: GetEmployee;
 }
 
 export const ActionsBlock: FC<Props> = ({ employeeData }): ReactElement => {
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const manager = useActionsBlockManagement();
+
+  const {
+    edit: { isEditModalOpen, handleEditModalClose, handleEditModalOpen },
+  } = manager;
+
+  const {
+    delete: {
+      isDeleteModalOpen,
+      handleDeleteModalClose,
+      handleDeleteModalOpen,
+    },
+  } = manager;
   return (
     <>
       <div className="employees__actions-block">
-        <Button
-          text="Edit"
-          type="button"
-          onClick={() => setIsEditModalOpen(true)}
-        />
-        <Button
-          text="Delete"
-          type="button"
-          onClick={() => setIsDeleteModalOpen(true)}
-        />
+        <Button text="Edit" type="button" onClick={handleEditModalOpen} />
+        <Button text="Delete" type="button" onClick={handleDeleteModalOpen} />
       </div>
       <EditEmployeeModal
         employeeData={employeeData}
         isOpen={isEditModalOpen}
-        handleClose={() => setIsEditModalOpen(false)}
+        handleClose={handleEditModalClose}
       />
       <DeleteEmployeeModal
         employeeId={employeeData.id}
         isOpen={isDeleteModalOpen}
-        handleClose={() => setIsDeleteModalOpen(false)}
+        handleClose={handleDeleteModalClose}
       />
     </>
   );
