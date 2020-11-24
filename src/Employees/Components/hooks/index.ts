04b08f7f3b95e4a1df2c4f2fd5905df66ef9e95a
Austral1a <lienglex@gmail.com>
2020-11-24 14:45:20 +0200

    add: useEditEmployeeManagement hook

--- src/Employees/Components/hooks/index.ts
+++ src/Employees/Components/hooks/index.ts
@@ -1,2 +1,3 @@
 export * from "./useDeleteEmployeeManagement";
 export * from "./useActionsBlockManagement";
+export * from "./useEditEmployeeManagement";
---
+++ src/Employees/Components/hooks/useEditEmployeeManagement.tsx
@@ -0,0 +1,31 @@
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { editEmployeeAction } from "../../../Store/Employees";
+import { Employee, GetEmployee } from "../../../Interfaces/Employees";
+import { FormikProps } from "formik";
+
+interface Props {
+  employeeData: GetEmployee;
+  handleClose: () => void;
+}
+
+export const useEditEmployeeManagement = ({
+  employeeData,
+  handleClose,
+}: Props) => {
+  const dispatch = useDispatch();
+  const { id } = employeeData;
+
+  const dispatchAction = useCallback(
+    (props: FormikProps<Employee>) =>
+      dispatch(editEmployeeAction({ ...props.values, id })),
+    [dispatch, id]
+  );
+
+  return {
+    ...employeeData,
+    btnText: "Edit",
+    handleClose,
+    dispatchAction,
+  };
+};
