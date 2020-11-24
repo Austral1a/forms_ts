2ac5fce93ff1d1c9dbce80d6eb4a909926dac829
Austral1a <lienglex@gmail.com>
2020-11-24 15:22:50 +0200

    add: useEmployeesListManagement hook

--- src/Employees/Components/hooks/index.ts
+++ src/Employees/Components/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from "./useDeleteEmployeeManagement";
 export * from "./useActionsBlockManagement";
 export * from "./useEditEmployeeManagement";
+export * from "./useEmployeesListManagement";
---
+++ src/Employees/Components/hooks/useEmployeesListManagement.tsx
@@ -0,0 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import { GetEmployee } from "../../../Interfaces/Employees";
+import { createSelector } from "reselect";
+import { getEmployeesAction } from "../../../Store/Employees";
+import { useEffect } from "react";
+
+export const useEmployeesManagement = () => {
+  const dispatch = useDispatch();
+
+  const selectEmployees = createSelector(
+    //TODO: Create state type
+    (state: any) => state.getEmployeesReducer.employees,
+    (employees) => employees
+  );
+
+  useEffect(() => {
+    dispatch(getEmployeesAction());
+  }, [dispatch]);
+  const employees: GetEmployee[] = useSelector(selectEmployees);
+  return employees;
+};
