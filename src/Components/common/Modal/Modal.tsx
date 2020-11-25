6f04185abcc5973562f8a0fd3c9d19252855f9cc
Austral1a <lienglex@gmail.com>
2020-11-25 15:33:30 +0200

    del: useless constant

--- src/Components/common/Modal/Modal.tsx
+++ src/Components/common/Modal/Modal.tsx
@@ -2,7 +2,6 @@ import React, { FC, ReactElement, ReactNode, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./Modal.scss";
 
-const appRoot = document.querySelector("#root") as HTMLDivElement;
 const modalRoot = document.querySelector("#modal-root") as HTMLDivElement;
 
 interface Props {
