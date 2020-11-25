e9116767da721eb920e1230d5170d8bfc6235c83
Austral1a <lienglex@gmail.com>
2020-11-25 15:42:58 +0200

    redner ErrorHandler

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { Login } from "./Login";
 import { Employees } from "./Employees";
 import { useSelector } from "react-redux";
+import { ErrorHandler } from "./ErrorHandler";
 
 export const Main: FC = () => {
   const isUserLoggedIn = useSelector(
@@ -11,21 +12,26 @@ export const Main: FC = () => {
   );
 
   return (
-    <Employees />
-    /*<Router>
-    <Route path="/">
-      {isUserLoggedIn ? (
-        <Redirect to="/employees" />
-      ) : (
-        <Redirect to="/login" />
-      )}
-    </Route>
-    <Route path="/login">
-      <Login />
-    </Route>
-    <Route path="/employees">
+    <>
+      <ErrorHandler />
       <Employees />
-    </Route>
-  </Router>*/
+      {/*
+      <Router>
+        <Route path="/">
+          {isUserLoggedIn ? (
+            <Redirect to="/employees" />
+          ) : (
+            <Redirect to="/login" />
+          )}
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/employees">
+          <Employees />
+        </Route>
+      </Router>
+      */}
+    </>
   );
 };
