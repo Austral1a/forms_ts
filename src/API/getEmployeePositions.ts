2cad7eecb1e6635bc8f84276812d41e0e6b37bc5
Austral1a <lienglex@gmail.com>
2020-11-30 10:16:10 +0200

    add: get employee positions api func

---
+++ src/API/getEmployeePositions.ts
@@ -0,0 +1,13 @@
+import { home } from "../url";
+import { GetEmployeePositionsSuccessPayload } from "@StoreEmployees";
+
+export const getEmployeePositions = async (): Promise<
+  GetEmployeePositionsSuccessPayload | Error
+> => {
+  const resource = `${home}/meta/employee_positions/`;
+  const response = await fetch(resource);
+  if (!response.ok) {
+    throw new Error("Failed to fetch employee positions");
+  }
+  return await response.json();
+};
