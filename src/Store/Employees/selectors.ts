21494448b8450ad95d4f1177761919111d9f35db
Austral1a <lienglex@gmail.com>
2020-11-30 10:36:12 +0200

    add: employee positions selector

--- src/Store/Employees/selectors.ts
+++ src/Store/Employees/selectors.ts
@@ -1,7 +1,12 @@
 import { createSelector } from "reselect";
-import { State, EmployeeAwareState } from "./interfaces";
-
+import { EmployeeAwareState, EmployeePositionsAwareState } from "./interfaces";
 export const selectEmployees = createSelector(
   (state: EmployeeAwareState) => state.employeesReducer.employees,
   (employees) => employees
+);
+
+export const selectEmployeePositions = createSelector(
+  (state: EmployeePositionsAwareState) =>
+    state.employeePositionsReducer.employee_positions,
+  (employee_positions) => employee_positions
 );
