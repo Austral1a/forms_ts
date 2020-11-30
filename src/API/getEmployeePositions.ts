0119fef1a0d2bcb7142d0e1a2619712a86844b82
Austral1a <lienglex@gmail.com>
2020-11-30 11:25:23 +0200

    changed employee positions endpoint

--- src/API/getEmployeePositions.ts
+++ src/API/getEmployeePositions.ts
@@ -4,7 +4,7 @@ import { GetEmployeePositionsSuccessPayload } from "@StoreEmployees";
 export const getEmployeePositions = async (): Promise<
   GetEmployeePositionsSuccessPayload | Error
 > => {
-  const resource = `${home}/meta/employee_positions/`;
+  const resource = `${home}/meta?=employee_positions`;
   const response = await fetch(resource);
   if (!response.ok) {
     throw new Error("Failed to fetch employee positions");
